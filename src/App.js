af44720b13cd38fcc11052b2c46b7a1bbe4d4935
Arturmk099-creator <240287837+Arturmk099-creator@users.noreply.github.com>
2026-07-08 00:22:41 +0400

    Fix GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
-function App(props) {
+
+function App() {
     return (
         <div>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="/service" element={<Service/>}/>
-                  <Route path="/gallery" element={<Gallery/>}/>
-                  <Route path="/contact" element={<Contact/>}/>
-              </Routes>
-          </BrowserRouter>
+            <BrowserRouter basename="/Detailing">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/service" element={<Service />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
